e962e3d6ee5789fddbaa0e347f71360b310f9557
Dinis Cruz <dinis.cruz@owasp.org>
2019-04-03 21:29:40 +0100

    added first test

--- tests/test_Simple_Test.py
+++ tests/test_Simple_Test.py
@@ -6,4 +6,4 @@ from pbx_gs_python_utils.utils.Dev import Dev
 class test_Simple_Test(TestCase):
 
     def test_an_example(self):
-        Dev.pprint("**** This is my first test")
+        Dev.pprint("**** This is my first test....")
