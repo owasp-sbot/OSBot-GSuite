d4493ff4e15ef0bc621e96317268bf9a4f4e1d72
Dinis Cruz <dinis.cruz@owasp.org>
2019-04-10 06:51:01 +0100

    removed temp test

--- tests/test_osbot_suite.py
+++ tests/test_osbot_suite.py
@@ -7,14 +7,3 @@ class test_gsbot_suite(TestCase):
 
     def test__init__(self):
         assert type(osbot_gsuite.version_osbot_gsuite) is str
-
-
-    def test_CHECK_FILE_ACCESS(self):
-        from osbot_gsuite.apis.GSheets import GSheets
-        from pbx_gs_python_utils.utils.Dev import Dev
-
-        file_id = '1Zwzn1zc5xukhC9Sn1elWwXfHIIMghnXZHw0iOLy2x9s'
-
-        result = GSheets().sheets_metadata(file_id)
-
-        Dev.pprint(result)
