7f9f2479439fa512f41a10d25c1ab4d7e3f3e5bf
Dinis Cruz <dinis.cruz@owasp.org>
2019-05-31 06:30:56 +0100

    updated version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    version                       = "0.2.12"               , # change this on every release
+    version                       = "0.2.32"               , # change this on every release
     name                          = "osbot_gsuite"  ,
 
     author                        = "Dinis Cruz",
